8b1894611ae7105d88946150331004d11b80a64e
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-28 12:18:33 +0700

    Implementasi object DestinasiHome sebagai definisi rute halaman utama

---
+++ app/src/main/java/com/example/prak7/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak7.view.route
+import com.example.prak7.R
+
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
