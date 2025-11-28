2e71341e9a4c72c0499511288f3990e15ac656c7
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-28 12:18:42 +0700

    Implementasi object DestinasiEntry sebagai definisi rute halaman input siswa

---
+++ app/src/main/java/com/example/prak7/view/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.prak7.view.route
+import com.example.prak7.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
