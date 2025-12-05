3f3efec02492d4f40d18b18d02c3dec109c7277b
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-12-05 09:54:34 +0700

    Registrasi DetailViewModel dan EditViewModel ke dalam ViewModelFactory

--- app/src/main/java/com/example/prak7/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak7/viewmodel/provider/PenyediaViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.prak7.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak7.repositori.AplikasiSiswa
+import com.example.prak7.viewmodel.DetailViewModel
 import com.example.prak7.viewmodel.EntryViewModel
 import com.example.prak7.viewmodel.HomeViewModel
+import com.example.prak7.viewmodel.EditViewModel  // TAMBAHKAN IMPORT INI
 
 object PenyediaViewModel{
     val Factory = viewModelFactory{
@@ -15,6 +18,19 @@ object PenyediaViewModel{
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+        // TAMBAHKAN INI ⬇️⬇️⬇️
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
         }
     }
 }
